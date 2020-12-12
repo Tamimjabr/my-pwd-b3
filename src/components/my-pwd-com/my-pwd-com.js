74eb9c53630b1fe18ac817a25fc80e948b92c60c
Tamim Jabr <tj222kg@student.lnu.se>
2020-12-13 00:22:40 +0100

    start constructing my-pwd-com component

---
+++ src/components/my-pwd-com/my-pwd-com.js
@@ -0,0 +1,90 @@
+/**
+ * The custom element web component module.
+ *
+ * @author Tamim Jabr <tj222kg@student.lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Define template.
+ */
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+    #desktop{
+      background-color: #ffdd93;
+      height: 100vh;
+      width:100vw;
+      position: relative;
+    }
+    #desktopBar{
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100vw;
+      height: 40px;
+      background-color:  #c4b6b6;
+    }
+
+  </style>
+  <div id='desktop'>
+   <div id='desktopBar'></div>
+  </div>
+`
+
+/**
+ * Define custom element.
+ */
+customElements.define('my-pwd-com',
+  /**
+   *
+   */
+  class extends HTMLElement {
+    /**
+     * Creates an instance of the current type.
+     */
+    constructor () {
+      super()
+
+      // Attach a shadow DOM tree to this element and
+      // append the template to the shadow root.
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+      this.container = this.shadowRoot.querySelector('#desktop')
+    }
+
+    /**
+     * Watches the attributes "text" and "speed" for changes on the element.
+     *
+     * @returns {string[]} A string array of attributes to monitor.
+     */
+    static get observedAttributes () {
+      return []
+    }
+
+    /**
+     * Called by the browser engine when an attribute changes.
+     *
+     * @param {string} name of the attribute.
+     * @param {any} oldValue the old attribute value.
+     * @param {any} newValue the new attribute value.
+     */
+    attributeChangedCallback (name, oldValue, newValue) {
+
+    }
+
+    /**
+     * Called after the element is inserted into the DOM.
+     */
+    connectedCallback () {
+
+    }
+
+    /**
+     * Called after the element has been removed from the DOM.
+     */
+    disconnectedCallback () {
+
+    }
+  }
+)
