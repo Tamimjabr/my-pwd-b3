056b7059d339e034be6bc5545b219dd3684d97fe
Tamim Jabr <tj222kg@student.lnu.se>
2020-12-19 00:42:18 +0100

    prevent moving the window outside the desktop

--- src/components/my-window-com/my-window-com.js
+++ src/components/my-window-com/my-window-com.js
@@ -167,8 +167,13 @@ customElements.define(
         pos3 = e.clientX
         pos4 = e.clientY
         // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + 'px'
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px'
+        const sizes = elmnt.getBoundingClientRect()
+        console.log(sizes)
+        console.log(document.documentElement.clientWidth)
+        console.log(document.documentElement.clientHeight)
+
+        elmnt.style.top = (elmnt.offsetTop - pos2) < 0 ? 0 + 'px' : (elmnt.offsetTop - pos2) + 'px'
+        elmnt.style.left = (elmnt.offsetLeft - pos1) < 0 ? 0 + 'px:' : (elmnt.offsetLeft - pos1) + 'px'
       }
 
       /**
